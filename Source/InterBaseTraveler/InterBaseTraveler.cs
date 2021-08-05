d76d3a45259835c1a9a4dbb4b276684e20255815
lucas861223 <lucas861223@gmail.com>
2021-08-05 12:05:58 -0700

    added some Defs

--- Source/InterBaseTraveler/InterBaseTraveler.cs
+++ Source/InterBaseTraveler/InterBaseTraveler.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
 
 namespace InterBaseTraveler
 {
-    public class Class1
+    public class InterBaseTraveler
     {
     }
 }
